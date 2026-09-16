4b8009f5d7b7a3fe55225effc3e4d4b5823faf70
tharsika08 <tharsikarangaraju717@gmail.com>
2026-09-16 23:20:50 +0530

    Send JWT token with axios requests

--- food-ordering-app/frontend/src/api/axios.js
+++ food-ordering-app/frontend/src/api/axios.js
@@ -1,5 +1,18 @@
 import axios from "axios";
 
-export default axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
+
+// Every request-ku token automatically send aagum
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default api;
