a5dec4637397bc9bbe2ee5536a534d1a1dbc7df1
tharsika08 <tharsikarangaraju717@gmail.com>
2026-09-16 10:33:49 +0530

    Connect frontend to deployed backend

--- food-ordering-app/frontend/src/api/axios.js
+++ food-ordering-app/frontend/src/api/axios.js
@@ -1,11 +1,5 @@
-import axios from 'axios';
+import axios from "axios";
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
-
-api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+export default axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
 });
-
-export default api;
